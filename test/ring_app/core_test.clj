b187a8beb294bc7fbe79ace58b1a0000f169c1eb
Jared Langson <langson.jared@gmail.com>
2021-04-15 11:26:52 -0400

    Revert test from core2 to core

--- test/ring_app/core_test.clj
+++ test/ring_app/core_test.clj
@@ -1,6 +1,6 @@
 (ns ring-app.core-test
   (:require [clojure.test :refer :all]
-            [ring-app.core2 :refer :all]))
+            [ring-app.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
