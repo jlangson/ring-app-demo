88a14ef99a6037b21235c4ac470609425fa67bf3
Jared Langson <langson.jared@gmail.com>
2021-04-15 09:11:37 -0400

    Enable reload

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,15 +1,27 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler [request-map]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> your IP is: "
-              (:remote-addr request-map)
-              "</body></html>")})
+  (response/response
+    (str "<html><body> your IP is: "
+         (:remote-addr request-map)
+         ;"\nthe scheme type is "
+         ;(:scheme request-map)
+         "</body></html>")))
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
 
 (defn -main []
   (jetty/run-jetty
-    handler
+    (-> handler
+        var
+        wrap-nocache
+        wrap-reload)
     {:port 3000
      :join? false}))
